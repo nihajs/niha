1aa3fbc909046080540ab1dead4f8a67cb36c2cf
Nico385412 <nico.hansse@gmail.com>
2024-01-30 00:10:41 +0100

    chore: add protobuf as serializer

--- packages/json-analyzer/README.md
+++ packages/json-analyzer/README.md
@@ -5,8 +5,25 @@ A simple library to determine which stringify library is faster:
 - JSON.stringify
 - [fast-json-stable-stringify](https://www.npmjs.com/package/fast-json-stable-stringify)
 - [fast-json-stringify](https://www.npmjs.com/package/fast-json-stringify)
+- [protobufjs](https://www.npmjs.com/package/protobufjs)
 ## Usage
 
-Run `npx @niha-js/json-analyzer jsonFile.json jsonSchemaFile.json`
+Run `npx @niha-js/json-analyzer bench jsonFile.json --jsonSchema jsonSchemaFile.json --proto file.proto --protoMessage ProtoMessage`
 
 > Please note: jsonSchemaFile.json is optional but recommended to get get better results for fast-json-stringify
+
+## Help
+
+```
+Usage: Json Analyser [options] [command]
+
+Benchmark helper to choose which JSON serializer is good for your payload
+
+Options:
+  -V, --version                   output the version number
+  -h, --help                      display help for command
+
+Commands:
+  bench [options] <jsonFilePath>  Bench a JSON file and return the best serialization library
+  help [command]                  display help for command
+```
--- packages/json-analyzer/src/lib/json-analyzer.ts
+++ packages/json-analyzer/src/lib/json-analyzer.ts
@@ -3,45 +3,63 @@ import fs from "fs"
 import fastStringify from "fast-json-stable-stringify"
 import fastJSONStringify from "fast-json-stringify"
 import {Suite} from "benchmark"
+import { program } from "commander";
+import { version } from "../../package.json" assert { type: "json" };
+import protobufjs from "protobufjs"
 
-const [filename, jsonSchemaFileName] = process.argv.splice(2);
+program
+  .name("Json Analyser")
+  .description("Benchmark helper to choose which JSON serializer is good for your payload")
+  .version(version);
 
-if(!filename) {
-  console.log(`${chalk.bgRed.white("Error")} ${chalk.red("No file provided")}`)
-  console.log(`${chalk.bgBlack.white("Example usage :")}`)
-  console.log(`${chalk.bgBlack.white("npx @niha-js/json-analyzer path/to/json/file.json")}`)
-}
 
-const file = fs.readFileSync(filename).toString()
-let jsonSchema = {};
-if(jsonSchemaFileName) {
-  jsonSchema = JSON.parse(fs.readFileSync(jsonSchemaFileName).toString() || "{}")
-}
-const parser = fastJSONStringify(jsonSchema)
+program
+  .command("bench")
+  .description('Bench a JSON file and return the best serialization library')
+  .argument('<jsonFilePath>', 'the JSON file path')
+  .option('--jsonSchema <string>', 'jsonSchema file path')
+  .option('--proto <string>', 'proto file path')
+  .option('--protoMessage <string>', 'proto Message to use')
+  .action((filename, opts) => {
+    const file = fs.readFileSync(filename).toString()
+    let jsonSchema = {};
+    if(opts.jsonSchema) {
+      jsonSchema = JSON.parse(fs.readFileSync(opts.jsonSchema).toString() || "{}")
+    }
 
-// console.time(`JSON.parse`)
-const json = JSON.parse(file)
-// console.timeEnd(`JSON.parse`)
+    const parser = fastJSONStringify(jsonSchema)
+    const json = JSON.parse(file)
+    const suite = new Suite()
 
-const suite = new Suite()
+    if(opts.proto && opts.protoMessage) {
+      const proto = protobufjs.loadSync(opts.proto).root.lookupType(opts.protoMessage);
+      suite.add('Protobuf', function() {
+        const encodedMessage = proto.create(json);
+        proto.encode(encodedMessage).finish()
+      })
+    }
 
-suite.add('JSON.stringify', function() {
-  JSON.stringify(json)
-})
-.add('fast-json-stable-stringify', function() {
-  fastStringify(json)
-})
-.add('fast-json-stringify', function() {
-  parser(json)
-})
-// add listeners
-  //@ts-ignore
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  //@ts-ignore
-  console.log('Fastest library for your case is ✨ ' + chalk.bgGreenBright.white(this.filter('fastest').map('name')) + ' ✨');
-})
-// run async
-.run({ 'async': true });
+
+    suite.add('JSON.stringify', function() {
+      Buffer.from(JSON.stringify(json))
+    })
+    .add('fast-json-stable-stringify', function() {
+      Buffer.from(fastStringify(json))
+    })
+    .add('fast-json-stringify', function() {
+      Buffer.from(parser(json))
+    })
+    // add listeners
+      //@ts-ignore
+    .on('cycle', function(event) {
+      console.log(String(event.target));
+    })
+    .on('complete', function() {
+      //@ts-ignore
+      console.log('Fastest library for your case is ✨ ' + chalk.bgGreenBright.white(this.filter('fastest').map('name')) + ' ✨');
+    })
+    // run async
+    .run({ 'async': true });
+  })
+
+program.parse();
